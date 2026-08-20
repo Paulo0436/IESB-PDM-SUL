c09f8a96a1e1d5aa6ea5438f6cad3f7b552f2301
unknown <2424290051@academico.iesb.br>
2026-08-19 21:31:58 -0300

    Adicionando aula de hoje

--- praticas/pratica03/app-atividade-aula-tarefas/App.js
+++ praticas/pratica03/app-atividade-aula-tarefas/App.js
@@ -1,26 +1,47 @@
-import { StyleSheet, Text, TextInput, View  , Button} from 'react-native';
-import { rotulo_input_meta, rotulo_btn_cadastro_meta , rotulo_lista_metas } from './mensagem';
+import { StyleSheet, Text, TextInput, View, Button } from 'react-native';
+import { rotulo_input_meta, rotulo_btn_cadastro_meta, rotulo_lista_metas } from './mensagem';
 
 export default function App() {
   return (
-    <View style={styles.mainConteiner}>
-      <TextInput placeholder={rotulo_input_meta}/>
-      <Button title={rotulo_btn_cadastro_meta}/>
-      <Text>{rotulo_lista_metas}</Text>
-   
+    <View style={styles.mainContainer}>
+      <View style={{ flexDirection: 'row', justifyContent: 'space-between', flex: 1 }}>
+        <View style={{ width: '65%' }}>
+          <TextInput
+            style={styles.inputText}
+            placeholder={rotulo_input_meta}
+          />
+        </View>
+
+        <View style={{ width: '30%' }}>
+          <Button title={rotulo_btn_cadastro_meta} />
+        </View>
+      </View>
+
+      <View style={styles.metaContainer}>
+        <Text>{rotulo_lista_metas}</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
-  mainConteiner:{
-    padding: 30 , 
 
-  }
+  mainContainer: {
+    padding: 30,
+    flex: 1,
+    flexDirection: 'column',
+  },
+
+  inputText: {
+    borderColor: '#cccccc',
+    borderWidth: 1,
+  },
+
+  metaContainer: {
+    flex: 1,
+  },
 });
